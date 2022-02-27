e8a41ff6c04f7238c019a9c92f85d89ec6c55c42
naca-nyan <naca.nyan@gmail.com>
2022-02-28 07:51:37 +0900

    Add reciever

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,11 @@
     windows_subsystem = "windows"
 )]
 
-use rosc::encoder;
+use rosc::{decoder, encoder};
 use rosc::{OscMessage, OscPacket, OscType};
+use std::collections::HashMap;
 use std::net::UdpSocket;
+use std::thread;
 
 #[tauri::command]
 fn send_osc_message(addr: String, value: String, typ: String) {
@@ -20,10 +22,32 @@ fn send_osc_message(addr: String, value: String, typ: String) {
         args: vec![OscType::Int(value)],
     }))
     .expect("cannot encode");
-    sock.send(&buf);
+    let _res = sock.send(&buf).expect("cannot send");
+}
+
+fn reciever() {
+    let mut args = HashMap::new();
+    let socket = UdpSocket::bind("127.0.0.1:9001").expect("couldn't bind to address");
+    let mut buf = [0; decoder::MTU];
+    loop {
+        println!("-----------");
+        let (len, _addr) = socket.recv_from(&mut buf).expect("Didn't receive data");
+        let filled_buf = &buf[..len];
+        let packet = decoder::decode(&filled_buf).expect("failed to decode");
+        match packet {
+            OscPacket::Message(msg) => {
+                args.insert(msg.addr, msg.args);
+            }
+            _ => {}
+        }
+        for (k, v) in args.iter() {
+            println!("{}: {:?}", k, v)
+        }
+    }
 }
 
 fn main() {
+    thread::spawn(reciever);
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler!(send_osc_message))
         .run(tauri::generate_context!())
