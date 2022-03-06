1d08584933b8f8e3839bcc2ea8c165fd95972882
naca-nyan <naca.nyan@gmail.com>
2022-03-06 21:21:35 +0900

    Add int and bool

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,13 @@ import { invoke } from "@tauri-apps/api/tauri";
 type Typ = "Int" | "Bool" | "Float";
 
 interface Parameter {
-  address: string;
+  addr: string;
   typ: Typ;
 }
 
 const parameters = ref<Parameter[]>([]);
 
-const addressBase = ref("/avatar/parameters/");
+const addrBase = ref("/avatar/parameters/");
 const paramDefault = "VRCEmotes";
 const parameter = ref("");
 const typ = ref("Int");
@@ -19,14 +19,15 @@ const typ = ref("Int");
 function addParameter() {
   const actualParam = parameter.value ? parameter.value : paramDefault;
   const param = {
-    address: addressBase.value + actualParam,
+    addr: addrBase.value + actualParam,
     typ: typ.value as Typ,
   };
   parameters.value.push(param);
 }
 
 async function send(param: Parameter, value: string) {
-  const body = { value, ...param };
+  const body = { ...param, value };
+  console.log(body);
   await invoke("send_osc_message", body);
 }
 
@@ -47,7 +48,7 @@ async function recieveLoop() {
 </script>
 
 <template>
-  <header class="container-fluid p-5 bg-primary text-white text-center">
+  <header class="container-fluid p-4 bg-primary text-white text-center">
     <h1>OSC for VRChat</h1>
   </header>
   <main class="container">
@@ -57,7 +58,7 @@ async function recieveLoop() {
           <div class="col-8">
             <label class="visually-hidden">Parameter</label>
             <div class="input-group">
-              <input v-model="addressBase" class="input-group-text" />
+              <input v-model="addrBase" class="input-group-text" />
               <input
                 v-model="parameter"
                 class="form-control"
@@ -78,8 +79,46 @@ async function recieveLoop() {
           </button>
         </div>
         <hr />
-        <div v-for="p in parameters" class="mb-3 mt-3">
-          {{ p }}
+        <div v-for="p in parameters" class="mb-3">
+          <div class="row mb-1">
+            <div class="col form-label">
+              {{ p.addr }}
+              <span class="badge rounded-pill bg-secondary text-light">
+                {{ p.typ }}
+              </span>
+            </div>
+          </div>
+          <div class="row">
+            <div v-if="p.typ === 'Int'" class="btn-group">
+              <button
+                v-for="i in ['0', '1', '2', '3', '4', '5', '6']"
+                @click="send(p, i)"
+                class="btn btn-outline-primary"
+              >
+                {{ i }}
+              </button>
+            </div>
+            <div v-else-if="p.typ === 'Bool'" class="btn-group">
+              <button @click="send(p, 'True')" class="btn btn-outline-primary">
+                True
+              </button>
+              <button @click="send(p, 'False')" class="btn btn-outline-primary">
+                False
+              </button>
+            </div>
+            <div v-else-if="p.typ === 'Float'" class="input-group">
+              <input
+                type="number"
+                value="1.0"
+                step="0.1"
+                class="form-control"
+              />
+              <button class="btn btn-outline-primary col-3" disabled>
+                (not implemented)
+              </button>
+            </div>
+          </div>
+          <hr />
         </div>
       </div>
       <div class="mt-4 col-lg-6">
