3cfee0579c75793d7799c6127da8ee06974b3504
naca-nyan <naca.nyan@gmail.com>
2022-03-17 03:24:13 +0900

    Add username and room

--- src/components/ConnectForm.vue
+++ src/components/ConnectForm.vue
@@ -8,15 +8,24 @@ const emit = defineEmits<{
   (e: "onmessage", message: MessageEvent<any>): void;
 }>();
 
+const user = ref("");
+const room = ref("");
+
 const serverAddr = ref("");
 const serverAddrDefault = "wss://";
 
 let sock: null | WebSocket = null;
 const state = ref<ReadyState>("CLOSED");
-function connect(url: string, protocols?: string) {
+function connect() {
   state.value = "CONNECTING";
   try {
-    sock = new WebSocket(url, protocols);
+    if (!user.value) throw new Error("おなまえがないよ～");
+    if (!room.value) throw new Error("あいことばがないよ～");
+    if (!serverAddr.value) throw new Error("さーばーあどれすがないよ");
+    const url = new URL(serverAddr.value);
+    const params = new URLSearchParams({ user: user.value, room: room.value });
+    url.search = params.toString();
+    sock = new WebSocket(url);
     sock.onopen = (_) => (state.value = "OPEN");
     sock.onmessage = (message) => emit("onmessage", message);
     sock.onerror = (err) => {
@@ -25,7 +34,7 @@ function connect(url: string, protocols?: string) {
     };
   } catch (e: any) {
     state.value = "CLOSED";
-    addLogs(!url ? "Invalid url: No URL supplied" : e.toString());
+    addLogs(e.toString());
   }
 }
 
@@ -57,7 +66,7 @@ function addLogs(log: string) {
 function onkeydown(e: KeyboardEvent) {
   if (e.ctrlKey || e.metaKey) {
     if (e.key === "Enter") {
-      connect(serverAddr.value);
+      connect();
     }
   }
 }
@@ -68,6 +77,28 @@ defineExpose({
 });
 </script>
 <template>
+  <div class="row">
+    <div class="col-sm-8 m-auto mt-2">
+      <div class="row">
+        <div class="col-6 pe-1">
+          <input
+            v-model="user"
+            class="form-control"
+            :disabled="state !== 'CLOSED'"
+            placeholder="おなまえ"
+          />
+        </div>
+        <div class="col-6 ps-1">
+          <input
+            v-model="room"
+            class="form-control"
+            :disabled="state !== 'CLOSED'"
+            placeholder="あいことば"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="row">
     <div class="col-sm-8 m-auto mt-2">
       <label class="visually-hidden">Server Address</label>
@@ -81,7 +112,7 @@ defineExpose({
         />
         <button
           v-if="state === 'CLOSED'"
-          @click="connect(serverAddr)"
+          @click="connect()"
           class="btn btn-info"
         >
           Connect
