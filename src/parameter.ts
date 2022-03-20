cb9ea7b0d7189585631bc130f9703960496c5bd4
naca-nyan <naca.nyan@gmail.com>
2022-03-20 22:26:16 +0900

    Delete manual send

--- src/parameter.ts
+++
@@ -1,8 +0,0 @@
-export const parameterTypes = ["Int", "Bool", "Float"] as const;
-
-export type ParameterType = typeof parameterTypes[number];
-
-export interface Parameter {
-  addr: string;
-  typ: ParameterType;
-}
